591faa6926ea6eef29df82fb2e9ca92262d3f01e
Augusto Sanchez <a3.diaz@outlook.com>
2023-02-11 11:39:13 -0600

    -Avances generales

---
+++ Blaztrap.Controls.Surface/BzModal.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blaztrap.Controls;
+
+public partial class BzModal
+{
+    [Parameter]
+    public string? TitleText { get; set; }
+
+    [Parameter]
+    public RenderFragment? Body { get; set; }
+
+    [Parameter]
+    public RenderFragment? Footer { get; set; }
+}
